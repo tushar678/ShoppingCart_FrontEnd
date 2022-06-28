61a1916193dbaceac05e8525f7a96a612c351db8
tushar <tushar@VE23062021NSEZ->
2022-06-28 19:08:06 +0530

    Dropdown Feature Added

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BookserviceService } from 'src/app/services/Books/books.service';
+import { BookserviceService } from 'src/app/services/books.service';
+import { MenuService } from 'src/app/services/menu.service';
 import { BookDetails } from 'src/app/shared/data/BookDetails';
 import { productsDB } from '../../shared/data/products';
 
@@ -13,8 +14,16 @@ export class ProductListComponent implements OnInit {
   advanceSearchExpanded: boolean = false;
   products = [];
   bookData:BookDetails[] = [];
+  menuList = [];
+  booksBySubMenu = [];
+  ITMenu = [];
+  ProfMenu = [];
+  subMenuSelected: any;
   
-  constructor(private bookService: BookserviceService) {}
+  constructor(
+    private bookService: BookserviceService,
+    private menuService: MenuService
+    ) {}
 
   ngOnInit(): void {
     setTimeout(() => {
@@ -22,12 +31,56 @@ export class ProductListComponent implements OnInit {
       this.isLoaded = true
     }, 1000)
     this.getAllBooks();
+    this.getMenuList();
+    this.getITSubMenu();
+    this.getProfSubMenu();
   }
 
   getAllBooks(){
     this.bookService.getBooks().subscribe((data: any)=> {
       this.bookData = data;
-      console.log(this.bookData);
+      console.log('Bookdata', this.bookData);
     })
   }
+
+  //menu name displayed hard-coded in HTML
+  getMenuList(){
+    this.menuService.getMenuList().subscribe((data: any)=> {
+      this.menuList = data;
+      console.log('Menu List', this.menuList);
+    })
+  }
+
+  getBooksbySubMenu(){
+    // to be made dynamic by selecting from dropdown
+    this.subMenuSelected = 'AWS';
+    this.menuService.getBooksBySubMenu(this.subMenuSelected).subscribe((data: any)=> {
+      this.booksBySubMenu = data;
+      console.log('Books under',this.subMenuSelected, 'are :', this.booksBySubMenu);
+    })
+  }
+
+  //get sub menu list for menus hard-coded
+  getITSubMenu(){
+    this.menuService.getSubMenuList('IT CERTIFICATIONS').subscribe((data: any)=>{
+      this.ITMenu = data;
+      console.log('IT Sub Menu', this.ITMenu);
+    })
+  } 
+  
+  getProfSubMenu(){
+    this.menuService.getSubMenuList('PROFESSIONAL CERTIFICATIONS').subscribe((data: any)=>{
+      this.ProfMenu = data;
+      console.log('Prof Sub Menu', this.ProfMenu);
+    })
+  }
+  
+  show(menu: string){
+    this.subMenuSelected = menu;
+    console.log("Selected sub menu", this.subMenuSelected);
+    this.menuService.getBooksBySubMenu(this.subMenuSelected).subscribe((data: any)=> {
+      this.bookData = data;
+    })
+  }
+
 }
---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  constructor(private http: HttpClient) { }
+
+  baseUrl = environment.baseUrl;
+
+  getMenuList(){
+    return this.http.get(this.baseUrl + 'Menus/GetMenus');
+  }
+
+  getSubMenuList(name: string){
+    return this.http.get(this.baseUrl + 'Menus/GetSubMenuByName?name=' + name);
+  }
+
+  getBooksBySubMenu(name: string){
+    return this.http.get(this.baseUrl + 'Menus/GetBooksBySubMenu?menu=' + name);
+    }
+}
